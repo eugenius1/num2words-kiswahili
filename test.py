d046a8dba3d0ea75c267236855677070c454f8ea
Eusebius N. <eugenius1@users.noreply.github.com>
2015-09-22 20:30:15 +0100

    More test arguments
    including exponential, number string and text string

--- test.py
+++ test.py
@@ -1,11 +1,14 @@
 from num2words_sw import Number
 
-test_args = [-1234567.89, -3, -0.25, -0.005, 0 , 0.1, 3.14, 901,
-	 2015, 10099, 99000, 101000, 1000001, 100000017, 117000000,
-	103050709]
+def test():
+	test_args = [-1234567.89, -3, -0.25, -0.0005, 0 , 0.1,
+	 3.141592653589793238, 
+	 901, 2015, 10099, 99000, 101000, 1000001, 100000017, 117000000,
+	324050709, +5e8,
+	'420000000500','mambo']
 
-for n in test_args:
-    res = Number(n).convert_to_words()
-    print n, res
-    if res[-1] == ' ':
-		print '^......Terminating space'
+	for n in test_args:
+		res = Number(n).convert_to_words()
+		print n, res
+		if res and res[-1] == ' ':
+			print '^......Terminating space'
